38cc972ccb7a7f50efbec9b5e2f8d136eb4c8eac
Michael Gingras <mcg79@cornell.edu>
2019-11-26 22:12:58 -0500

    the radio actually works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        this is the yeecast, thank you for listening.
+        <audio controls src="http://192.168.1.205:8000/stream?type=.mp3" type="audio/mpeg">
+            your browser does not support the audio element. nerd.
+        </audio>
       </header>
     </div>
   );
