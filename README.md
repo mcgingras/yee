30cc2c0e45750f53823f2b5c880e361ce05b79b7
Michael Gingras <mcg79@cornell.edu>
2019-11-26 23:50:54 -0500

    new page

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Vitamin Yee
 Best online radio station out there, ever. Nice!
 
-/usr/local/etc/icecast.xml
+
+### How to Start Stream
+
+- icecast -c /usr/local/etc/icecast.xml
+- make sure IP address is right in BUTT (and on this website)
+- run butt
+- run linein
--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import './App.css';
+import sb from './sb2.gif';
 
 function App() {
+  let audio = useRef(null);
+
+  useEffect(() => {
+    console.log(audio.current.error);
+  }, [])
+
+  const getAudio = () => {
+    if(audio.current.error){
+      console.log(audio.current.error);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        this is the yeecast, thank you for listening.
-        <audio controls src="http://192.168.1.205:8000/stream?type=.mp3" type="audio/mpeg">
+        <h1>YEEEEEEEEEEEE!!!!!!!</h1>
+        <img src={sb} />
+        <p onClick={() => {getAudio()}}>(spoons rattling)</p>
+        <audio ref={audio} controls src="http://192.168.1.205:8000/stream?type=.mp3" type="audio/mpeg">
             your browser does not support the audio element. nerd.
         </audio>
       </header>
