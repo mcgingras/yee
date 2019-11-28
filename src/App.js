a7c7398b852e60cdee03bf38d565af2725580f4e
Michael Gingras <mcg79@cornell.edu>
2019-11-28 18:50:31 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import React, {useRef, useEffect} from 'react';
+import React from 'react';
 import './App.css';
-import sb from './sb2.gif';
+import cows from './cows.svg';
 
 function App() {
-  let audio = useRef(null);
-
-  useEffect(() => {
-    console.log(audio.current.error);
-  }, [])
-
-  const getAudio = () => {
-    if(audio.current.error){
-      console.log(audio.current.error);
-    }
-  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>YEEEEEEEEEEEE!!!!!!!</h1>
-        <img src={sb} />
-        <p onClick={() => {getAudio()}}>(spoons rattling)</p>
-        <audio ref={audio} controls src="http://192.168.1.205:8000/stream?type=.mp3" type="audio/mpeg">
-            your browser does not support the audio element. nerd.
-        </audio>
+        <div>
+          <h1>Vitamin Yee</h1>
+          <audio controls src="http://192.168.1.205:8000/stream?type=.mp3" type="audio/mp3">
+              your browser does not support the audio element. nerd.
+          </audio>
+        </div>
+        <div>
+          <img src={cows} />
+        </div> 
       </header>
     </div>
   );
