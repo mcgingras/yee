f0fa0dcef91cde0fdcc35932e29deb557b3e522a
Michael Gingras <mcg79@cornell.edu>
2019-11-28 20:36:34 -0500

    lets try this

--- src/App.js
+++ src/App.js
@@ -4,18 +4,13 @@ import cows from './cows.svg';
 
 function App() {
 
-  let audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-  let audioElement = new Audio('http://192.168.1.205:8000/stream?type=.mp3'); // example stream
-  let audioSrc = audioCtx.createMediaElementSource(audioElement);
-  audioSrc.crossOrigin = 'anonymous';
-
   return (
     <div className="App">
       <header className="App-header">
         <div>
           <h1>Vitamin Yee</h1>
           <p>The best online radio station to bring that yee back in your life. We broadcast whenever we feel like it.</p>
-          <audio controls src="http://192.168.1.205:8000/stream?type=.mp3" type="audio/mp3" crossOrigin="anonymous">
+          <audio controls src="http://192.168.1.205:8000/stream" type="audio/mp3" crossOrigin="anonymous">
               your browser does not support the audio element. nerd.
           </audio>
         </div>
