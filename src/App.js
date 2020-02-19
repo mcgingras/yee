deee34a515f83d73b5c6b9c6e29f393839fa12b2
Michael Gingras <mcg79@cornell.edu>
2020-02-19 00:15:49 -0500

    does this work?

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import c2 from './c2.svg';
 import MusicPlayer from './components/MusicPlayer';
 
 function App() {
 
+  let [playing, setPlaying] = useState("Filler Artist Here")
+  let [isLive, setLive] = useState(true);
+
   return (
     <div>
       <div className="wrapper">
-        <div className="bg">
+        <div className="bg hide--mobile">
             {/* <img src={c2} /> */}
         </div> 
         <div>
-          <h6>Vitamin Yee Radio: Playlist Name </h6>
-          <h1 className="border-text">pluko</h1>
-          <h1>"I Want You"</h1> 
-          <audio controls src="http://3.20.167.55:8000/stream" type="audio/mp3">
+          <h6>{isLive ? 'Vitamin Yee Radio: Playlist Name' : 'Next Show: 2:10:34:22'}</h6>
+          <h1 className="border-text">{isLive ? playing : "Vitamin Yee"}</h1>
+          <h1>{isLive ? 'song title' : "Is Offline"}</h1> 
+          {/* <audio controls src="http://3.20.167.55:8000/stream" type="audio/mp3">
               your browser does not support the audio element. nerd.
-          </audio>
-          {/* <MusicPlayer /> */}
+          </audio> */}
+          {isLive ? <MusicPlayer /> : <p>see past playlists</p>}
         </div>
       </div>
       <footer>
--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function MusicPlayer(){
 
+    let [isPlaying, setPlaying] = useState(false);
+
+    const URL = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3';
+    const context = new AudioContext();
+    let yodelBuffer;
+    
+    fetch("http://3.20.167.55:8000/stream")
+    .then(response => response.body)
+    .then(body => {
+        const reader = body.getReader();
+        console.log(reader);
+        
+        let pump = () => {
+            reader.read().then(({value, done}) => {
+                // console.log(typeof(value));
+                // console.log(value);
+                
+                toAudioContext(value.buffer);
+                if(!done) pump()
+            })
+        }
+        pump()
+    })
+
+    const toAudioContext = (arrayBuffer) => {
+        context.decodeAudioData(arrayBuffer)
+        .then(audioBuffer => {
+            const source = context.createBufferSource();
+            source.buffer = audioBuffer;
+            source.connect(context.destination);
+            source.start();
+        })
+    }
+
+
+    // fetch(URL)
+    // .then(response => response.arrayBuffer())
+    // .then(arrayBuffer => context.decodeAudioData(arrayBuffer))
+    // .then(audioBuffer => {;
+    //     yodelBuffer = audioBuffer;
+    // });
+        
+    function play(audioBuffer) {
+        const source = context.createBufferSource();
+        source.buffer = audioBuffer;
+        source.connect(context.destination);
+        source.start();
+    }
+
+    const toggleAudio = () => {
+        play(yodelBuffer);
+        console.log(yodelBuffer);
+        
+    }
+
     return (
         <div className="player--wrapper">
-            <span className="player--play"></span>
+            <span className="player--play" onClick={() => {toggleAudio()}}></span>
             <div className="player--track">
                 <span className="player--current"></span>
             </div>
